0c00fc9d70f6fbf265f5ce3040ef56b0f3140775
rask1lll <satpekovrasul@gmail.com>
2025-10-13 02:32:41 +0500

    Add login form and update dependencies; remove unused SVGs

--- cloud-disc/src/app/page.tsx
+++ cloud-disc/src/app/page.tsx
@@ -1,103 +1,59 @@
-import Image from "next/image";
-
+"use client";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 export default function Home() {
+  const [passwordIsVisible, setPasswordIsVisible] = useState<boolean>(false);
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="w-dvw h-dvh flex justify-center font-semibold items-center">
+      <form
+        className="w-[50%] flex flex-col gap-5 p-6 rounded-2xl max-w-[350px] min-w-[260px] shadow-2xl bg-gray-100"
+        action=""
+      >
+        <h1 className="text-center text-2xl">Вход на платформу</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="px-2">
+          <label htmlFor="email" className="block">
+            Логин
+          </label>
+          <input
+            type="text"
+            placeholder="Пример логина"
+            id="email"
+            className="outline-0 p-2 ring-1 rounded-xl w-full ring-gray-400 bg-white"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="px-2">
+          <label htmlFor="password" className="block">
+            Пароль
+          </label>
+          <div className="p-2 ring-1 flex rounded-xl w-full ring-gray-400 bg-white">
+            <input
+              type={`${!passwordIsVisible ? "password" : "text"}`}
+              placeholder="Пример пароль"
+              id="email"
+              className="outline-0 w-[90%]"
+            />
+            {!passwordIsVisible ? (
+              <FaEye
+                className="h-[100%] hover:cursor-pointer w-6"
+                onClick={() => {
+                  setPasswordIsVisible(!passwordIsVisible);
+                }}
+              />
+            ) : (
+              <FaEyeSlash
+                className="h-[100%] hover:cursor-pointer w-6"
+                onClick={() => {
+                  setPasswordIsVisible(!passwordIsVisible);
+                }}
+              />
+            )}
+          </div>
+        </div>
+        <button className="p-3 px-6 rounded-2xl ring-1 ring-[#50505053] bg-[#aeecfa32]">
+          Вход
+        </button>
+      </form>
     </div>
   );
 }
