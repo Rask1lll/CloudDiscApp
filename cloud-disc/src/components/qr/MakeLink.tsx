399edc5b85e437f263a1cd7386aeb2c8bbd6d5f4
rask1lll <satpekovrasul@gmail.com>
2025-11-11 18:26:13 +0500

    feat: enhance link copying functionality with error handling and fallback method

--- cloud-disc/src/components/qr/MakeLink.tsx
+++ cloud-disc/src/components/qr/MakeLink.tsx
@@ -5,8 +5,25 @@ import { BsShare } from "react-icons/bs";
 export default function MakeLink({ link }: { link: string }) {
   const { setAlert } = useAlertStore();
   async function copyLink() {
-    setAlert({ label: "Ссылка была скопирована", color: "green" });
-    await navigator.clipboard.writeText(link);
+    try {
+      if (window.isSecureContext && navigator.clipboard) {
+        await navigator.clipboard.writeText(link);
+      } else {
+        const textarea = document.createElement("textarea");
+        textarea.value = link;
+        textarea.style.position = "fixed";
+        textarea.style.opacity = "0";
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand("copy");
+        document.body.removeChild(textarea);
+      }
+
+      setAlert({ label: "Ссылка была скопирована", color: "green" });
+    } catch (err) {
+      console.error("Ошибка копирования:", err);
+      setAlert({ label: "Не удалось скопировать ссылку", color: "red" });
+    }
   }
   return (
     <div>
